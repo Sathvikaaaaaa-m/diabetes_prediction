c36640b4764a24c2ef8fe83175f1c65da2c91e71
Sathvikaaaaaa-m <sathvika13406@gmail.com>
2025-05-30 22:19:58 +0530

    Update app.py

--- main-app/app.py
+++ main-app/app.py
@@ -267,5 +267,8 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, use_reloader=False, threaded=False)      
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+   
     
