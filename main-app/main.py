6b9694e4b29377ac8c0e1769478e60114b1019f5
Sathvikaaaaaa-m <sathvika13406@gmail.com>
2025-05-31 01:16:39 +0530

    Update main.py

--- main-app/main.py
+++ main-app/main.py
@@ -266,8 +266,5 @@ def predict():
         return jsonify({'error': 'Failed to get prediction from model API'}), 500
 
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+
     
